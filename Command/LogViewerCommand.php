ae0d2a5edd53a8ed78b9eb16b56e99fb9946bc18
Vazgen Manukyan <vazgen.manukyan@gmail.com>
2015-03-13 18:38:10 +0400

    added command

--- Command/LogViewerCommand.php
+++ Command/LogViewerCommand.php
@@ -16,7 +16,7 @@ use Dubture\Monolog\Reader\LogReader;
  *
  * @author Vazgen Manukyan <vazgen.manukyan@gmail.com>
  */
-class LogViewerCommand
+class LogViewerCommand extends ContainerAwareCommand
 {
 
     /**
